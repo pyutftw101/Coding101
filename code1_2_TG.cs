ddea442c12e717b5195075b60309a882f41709ce
Tiago-Gomes-F <111021148+Tiago-Gomes-F@users.noreply.github.com>
2022-12-16 16:29:53 +0000

    Update code1_2_TG.cs

--- code1_2_TG.cs
+++ code1_2_TG.cs
@@ -17,3 +17,31 @@ else
     maxResult = secondMax;
 }
 Console.WriteLine(maxResult);
+
+
+Console.WriteLine("Please insert first number: ");
+double thirdNumber = double.Parse(Console.ReadLine());
+Console.WriteLine("Please insert second number: ");
+double fourthNumber = double.Parse(Console.ReadLine());
+
+double divOne= thirdNumber/fourthNumber;
+double divTwo= fourthNumber/thirdNumber;
+
+Console.WriteLine(divOne.ToString("0.00"));
+Console.WriteLine(divTwo.ToString("0.00"));
+
+Console.WriteLine("Please insert first number: ");
+int fifthNumber = int.Parse(Console.ReadLine());
+Console.WriteLine("Please insert second number: ");
+int sixthNumber = int.Parse(Console.ReadLine());
+
+int multOne = fifthNumber * sixthNumber;
+
+if (multOne%2 == 0)
+{
+    Console.WriteLine("Bizz");
+}
+else
+{
+    Console.WriteLine("Buzz");
+}
