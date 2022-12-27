5ba9697d22b461fe985b23fa95ef7a08555b673b
pyutftw101 <103177327+pyutftw101@users.noreply.github.com>
2022-12-27 14:04:10 +0000

    Update Code1_3_GX.cs

--- Code1_3_GX.cs
+++ Code1_3_GX.cs
@@ -7,5 +7,18 @@ Console.Write("Please provide a number that you want to multiply: ");
         {
             Console.WriteLine($"{number} x {i} = {number * i}");
         }
-    }
-}
+
+// PARTE III //
+
+Console.Write("Please provide a number of lines: ");
+        int numLines = int.Parse(Console.ReadLine());
+        {
+            for (int i = 1; i <= numLines; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
