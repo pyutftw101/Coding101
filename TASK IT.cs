f071f11b78238d3e5d82cb62edfddf4820d78cdc
pyutftw101 <103177327+pyutftw101@users.noreply.github.com>
2023-01-20 10:22:13 +0000

    Create TASK IT.cs

---
+++ TASK IT.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+
+class ToDoTask
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public DateTime DateTime { get; set; }
+
+    public ToDoTask(string name, string description, DateTime dateTime)
+    {
+        Name = name;
+        Description = description;
+        DateTime = dateTime;
+    }
+}
+
+class ToDoList
+{
+    static void Main(string[] args)
+    {
+        List<ToDoTask> toDoList = new List<ToDoTask>();
+
+        while (true)
+        {
+            Console.WriteLine("1. Add task to list");
+            Console.WriteLine("2. Remove task from list");
+            Console.WriteLine("3. View list");
+            Console.WriteLine("4. Exit");
+
+            int choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    Console.Write("Enter task name: ");
+                    string name = Console.ReadLine();
+                    Console.Write("Enter task description: ");
+                    string description = Console.ReadLine();
+                    Console.Write("Enter task due date and time (yyyy-mm-dd hh:mm): ");
+                    DateTime dateTime = DateTime.Parse(Console.ReadLine());
+                    toDoList.Add(new ToDoTask(name, description, dateTime));
+                    Console.WriteLine("Task added!");
+                    break;
+                case 2:
+                    Console.Write("Enter task number to remove: ");
+                    int taskNum = int.Parse(Console.ReadLine());
+                    toDoList.RemoveAt(taskNum - 1);
+                    Console.WriteLine("Task removed!");
+                    break;
+                case 3:
+                    for (int i = 0; i < toDoList.Count; i++)
+                    {
+                        Console.WriteLine((i + 1) + ". " + toDoList[i].Name + " - " + toDoList[i].Description + " - " + toDoList[i].DateTime);
+                    }
+                    break;
+                case 4:
+                    return;
+                default:
+                    Console.WriteLine("Invalid choice. Please try again.");
+                    break;
+            }
+        }
+    }
+}
